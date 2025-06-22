88fc3c0911b55e58ed0862dcb641a7b40a7b6cc9
Kepa <pierre@ideestore.fr>
2025-06-22 23:40:39 +0200

    chore: renamed import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { HackathonCard } from "@/components/hackathon-card";
 import { BentoCard, BentoGrid } from "@/components/magicui/bento-grid";
 import BlurFade from "@/components/magicui/blur-fade";
 import BlurFadeText from "@/components/magicui/blur-fade-text";
-import { NewProjectCard, ProjectCard } from "@/components/project-card";
+import { ProjectCard } from "@/components/project-card";
 import { ResumeCard } from "@/components/resume-card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
